2b6e5fcfd269dd7487cc6d57d22dd6880df1de5e
MahmoudElSayed96 <gcfjxvkj@gmail.com>
2025-01-07 20:50:43 +0200

    add initial implementation of NFC record handling and form row components

---
+++ lib/core/helper/nfc_form_row.dart
@@ -0,0 +1,37 @@
+// import 'package:flutter/material.dart';
+
+// class FormRow extends StatelessWidget {
+//   const FormRow({
+//     super.key,
+//     required this.title,
+//     required this.value,
+//   });
+
+//   final Text title;
+//   final Text value;
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Row(
+//       children: <Widget>[
+//         title,
+//         value,
+//       ],
+//     );
+//   }
+// }
+
+// String getNdefType(String code) {
+//   switch (code) {
+//     case 'org.nfcforum.ndef.type1':
+//       return 'NFC Forum Tag Type 1';
+//     case 'org.nfcforum.ndef.type2':
+//       return 'NFC Forum Tag Type 2';
+//     case 'org.nfcforum.ndef.type3':
+//       return 'NFC Forum Tag Type 3';
+//     case 'org.nfcforum.ndef.type4':
+//       return 'NFC Forum Tag Type 4';
+//     default:
+//       return 'Unknown';
+//   }
+// }
