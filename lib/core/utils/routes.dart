f394d93ec89ad66db5273998a1201ba19f8fc4f8
MahmoudElSayed96 <gcfjxvkj@gmail.com>
2025-01-08 02:03:09 +0200

    add routes manager and custom app bar widget for navigation and UI consistency

---
+++ lib/core/utils/routes.dart
@@ -0,0 +1,7 @@
+class RoutesManager {
+  const RoutesManager._();
+
+  static const String landingScreen = '/';
+  static const String nfcReadDataScreen = '/nfc-read-data';
+  static const String nfcWriteDataScreen = '/nfc-write-data';
+}
---
+++ lib/core/widgets/app_bar.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
+  final String title;
+  final IconData icon;
+  const CustomAppBar({super.key, required this.title, required this.icon});
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      centerTitle: true,
+      title: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Icon(
+            icon,
+            size: 25.sp,
+            color: Colors.blue[300],
+          ),
+          SizedBox(width: 8.w),
+          Text(
+            title,
+            style: TextStyle(
+              fontSize: 22.sp,
+              color: Colors.black,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+}
